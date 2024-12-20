a92eab70430494cdbbf66b450deb77ad52f3756c
Mateusz Stompór <mateusz.stompor@gmail.com>
2024-12-20 13:19:19 +0100

    Thread rendering

--- mine/rtwriter.h
+++ mine/rtwriter.h
@@ -7,36 +7,91 @@
 
 #pragma once
 
+#include <Foundation/Foundation.h>
+
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+#include <random>
+
 #import "cgbitmap.h"
 #import "raytracer.h"
 
-#include <cstdint>
+template <typename T>
+struct ClosedRange {
+    T lowerBound;
+    T higherBound;
+};
+
+template <typename T>
+struct Region {
+    ClosedRange<T> x;
+    ClosedRange<T> y;
+};
+
 
 struct RTWriter {
-    RTWriter(uint16_t width, uint16_t height) : cgbitmap(1000, 1000, 4) {
-        
+    RTWriter(uint16_t width, uint16_t height, uint16_t regionSideSize = 100)
+    : cgbitmap(1000, 1000, 4)
+    , regionSideSize{regionSideSize} {
+        queue = [[NSOperationQueue alloc] init];
+        queue.maxConcurrentOperationCount = 8;
     }
-    
+    ~RTWriter() {
+        [queue release];
+    }
+        
     void capture(Scene const & scene) {
-        int raysPerPixel = 5;
+        std::vector<Region<uint16_t>> regions = divideIntoRegions();
+        std::random_device rd;
+        std::default_random_engine rng(rd());
+        std::shuffle(regions.begin(), regions.end(), rng);
+        
+        int raysPerPixel = 100;
         for (int i = 0; i < raysPerPixel; ++i) {
-            uint16_t width = cgbitmap.bitmap.width;
-            uint16_t height = cgbitmap.bitmap.height;
-            for (uint16_t y = 0; y < height; y++) {
-                for (uint16_t x = 0; x < width; x++) {
-                    
-                    Ray ray = scene.camera.ray(x, y, width, height);
-                    simd_float4 color = rt.trace(ray, scene, 10);
-                    
-                    
-                    simd_float4 currentColor = cgbitmap.bitmap.colorAt(x, height - y - 1);
-                    simd_float4 newColor = (currentColor * i + color) / float(i + 1);
-                    cgbitmap.bitmap.setNormalizedRGBA(x, height - y - 1, newColor);
-                }
+            for (Region<uint16_t> const & region : regions) {
+                [queue addOperationWithBlock:^{
+                    for (uint16_t x = region.x.lowerBound; x <= region.x.higherBound; x++) {
+                        for (uint16_t y = region.y.lowerBound; y <= region.y.higherBound; y++) {
+                            
+                            Ray ray = scene.camera.ray(x,
+                                                       y,
+                                                       cgbitmap.bitmap.width,
+                                                       cgbitmap.bitmap.height);
+                            simd_float4 color = rt.trace(ray, scene, 2);
+                            
+                            simd_float4 currentColor = cgbitmap.bitmap.colorAt(x, cgbitmap.bitmap.height - y - 1);
+                            simd_float4 newColor = (currentColor * i + color) / float(i + 1);
+                            cgbitmap.bitmap.setNormalizedRGBA(x, cgbitmap.bitmap.height - y - 1, newColor);
+                        }
+                    }
+                }];
+            }
+            [queue waitUntilAllOperationsAreFinished];
+        }
+    }
+    CGBitmap cgbitmap;
+    
+private:
+    std::vector<Region<uint16_t>> divideIntoRegions() {
+        std::vector<Region<uint16_t>> result{};
+        
+        for (uint16_t x = 0; x < cgbitmap.bitmap.width; x += regionSideSize) {
+            for (uint16_t y = 0; y < cgbitmap.bitmap.height; y += regionSideSize) {
+                uint16_t xMax = static_cast<uint16_t>(std::min(static_cast<uint16_t>(x + regionSideSize - 1),
+                                                               static_cast<uint16_t>(cgbitmap.bitmap.width - 1)));
+                ClosedRange<uint16_t> xRange{x, xMax};
+                uint16_t yMax = static_cast<uint16_t>(std::min(static_cast<uint16_t>(y + regionSideSize - 1),
+                                                               static_cast<uint16_t>(cgbitmap.bitmap.height - 1)));
+                ClosedRange<uint16_t> yRange{y, yMax};
+                result.push_back(Region<uint16_t>{xRange, yRange});
             }
         }
+        
+        return result;
     }
     
     RayTracer rt;
-    CGBitmap cgbitmap;
+    uint16_t regionSideSize;
+    NSOperationQueue * queue;
 };
