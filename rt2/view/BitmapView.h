1a27ea735e55df222fa065e0bf354978d6659889
Mateusz Stompór <mateusz.stompor@gmail.com>
2024-12-20 11:47:13 +0100

    Sample many rays

--- rt2/view/BitmapView.h
+++ rt2/view/BitmapView.h
@@ -46,13 +46,21 @@
 }
 
 - (void)generateBitmapData {
-    uint16_t width = cgbitmap->bitmap.width;
-    uint16_t height = cgbitmap->bitmap.height;
-    for (uint16_t y = 0; y < height; y++) {
-        for (uint16_t x = 0; x < width; x++) {
-            simd_float4 color = rt.trace(x, y, width, height, scene);
-            
-            cgbitmap->bitmap.setNormalizedRGBA(x, height - y - 1, color);
+    int raysPerPixel = 5;
+    for (int i = 0; i < raysPerPixel; ++i) {
+        uint16_t width = cgbitmap->bitmap.width;
+        uint16_t height = cgbitmap->bitmap.height;
+        for (uint16_t y = 0; y < height; y++) {
+            for (uint16_t x = 0; x < width; x++) {
+                
+                Ray ray = scene.camera.ray(x, y, width, height);
+                simd_float4 color = rt.trace(ray, scene, 10);
+                
+                
+                simd_float4 currentColor = cgbitmap->bitmap.colorAt(x, height - y - 1);
+                simd_float4 newColor = (currentColor * i + color) / float(i + 1);
+                cgbitmap->bitmap.setNormalizedRGBA(x, height - y - 1, newColor);
+            }
         }
     }
 }
