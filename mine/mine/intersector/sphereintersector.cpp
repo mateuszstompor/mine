538b6336cab2706038d566254be4f98468d6e329
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-04-11 12:45:03 +0200

    Implement closest intersection

--- mine/mine/intersector/sphereintersector.cpp
+++ mine/mine/intersector/sphereintersector.cpp
@@ -5,9 +5,12 @@
 //  Copyright © 2025 Mateusz Stompór. All rights reserved.
 //
 
+#include <vector>
+#include <algorithm>
+
 #include "sphereintersector.h"
 
-std::vector<float> mine::SphereIntersector::intersect(const Ray& ray, const Sphere& sphere, float epsilon) const {
+std::optional<float> mine::SphereIntersector::closestIntersection(const Ray& ray, const Sphere& sphere, float epsilon) const {
     auto oc = ray.origin - sphere.center;
     float k1 = simd::dot(ray.direction, ray.direction);
     float k2 = 2 * simd::dot(oc, ray.direction);
@@ -15,16 +18,27 @@ std::vector<float> mine::SphereIntersector::intersect(const Ray& ray, const Sphe
     float discriminant = k2 * k2 - 4 * k1 * k3;
     
     if (discriminant < 0) {
-        return {};  // No intersection
+        return std::nullopt;  // No intersection
     }
     
     if (std::abs(discriminant) < epsilon) {
         float t = -k2 / (2 * k1);
-        return {t};  // One intersection
+        return t >= 0 ? std::make_optional(t) : std::nullopt;  // One intersection
     }
     
     float sqrtDiscriminant = std::sqrt(discriminant);
     float t1 = (-k2 + sqrtDiscriminant) / (2 * k1);
     float t2 = (-k2 - sqrtDiscriminant) / (2 * k1);
-    return t1 > t2 ? std::vector<float>{t2, t1} : std::vector<float>{t1, t2};  // Return sorted intersections
+    std::vector<float> solutions;
+    solutions.reserve(2);
+    if (t1 > 0) {
+        solutions.push_back(t1);
+    }
+    if (t2 > 0) {
+        solutions.push_back(t2);
+    }
+    std::sort(solutions.begin(), solutions.end(), [](float a, float b) {
+        return a < b;
+    });
+    return solutions.empty() ? std::nullopt : std::make_optional(solutions[0]);
 }
