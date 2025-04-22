b32bcb4dbc5d60aeda8644886849bfe2509042ec
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-04-22 23:20:10 +0200

    Ensure intersection has proper values

--- mine/mine/scene/rayintersection.h
+++ mine/mine/scene/rayintersection.h
@@ -11,17 +11,42 @@
 
 #include <simd/simd.h>
 
+#include "../assertion/finite.h"
 #include "look/material.h"
 
 namespace mine {
     struct RayIntersection {
-        simd_float3 T;
-        simd_float3 B;
-        simd_float3 N;
-        simd_float3 point;
-        simd_float2 uv;
-        std::shared_ptr<Material> material = nullptr;
-        std::shared_ptr<simd::float3> lightColor = nullptr;
+        simd::float3 T;
+        simd::float3 B;
+        simd::float3 N;
+        simd::float3 point;
+        simd::float2 uv;
+        std::shared_ptr<Material> material;
+        std::shared_ptr<simd::float3> lightColor;
         float t;
+        RayIntersection(simd::float3 const & proposedT,
+                        simd::float3 const & proposedB,
+                        simd::float3 const & proposedN,
+                        simd::float3 const & proposedPoint,
+                        simd::float2 const & proposedUV,
+                        std::shared_ptr<Material> const & proposedMaterial,
+                        std::shared_ptr<simd::float3> const & proposedLightColor,
+                        float const & proposedTValue)
+        : T{proposedT}
+        , B{proposedB}
+        , N{proposedN}
+        , point{proposedPoint}
+        , uv{proposedUV}
+        , material{proposedMaterial}
+        , lightColor{proposedLightColor}
+        , t{proposedTValue}
+        {
+            assertFinite(proposedT);
+            assertFinite(proposedB);
+            assertFinite(proposedN);
+            assertFinite(proposedPoint);
+            assertFinite(proposedUV);
+            assert(simd::isfinite(proposedTValue));
+        }
     };
 }
