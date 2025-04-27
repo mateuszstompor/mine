611cf59b85552fe0d3e1808404e15a4402942776
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-04-27 23:30:28 +0200

    Calculate disk coordinate system once

--- mine/mine/coordinates/diskcoordinates.cpp
+++ mine/mine/coordinates/diskcoordinates.cpp
@@ -17,29 +17,20 @@ simd_float3 mine::DiskCoordinates::polarToCartesian(float r, float theta, Disk c
     assertInClosedRange(theta, 0.0f, 2.0f * static_cast<float>(M_PI));
     assertInClosedRange(r, 0.0f, disk.radius);
     assert(disk.normal.z != 0 && "disk.normal.z must not be zero to avoid division by zero");
-
     
     float x = r * cos(theta);
     float y = r * sin(theta);
     float z = 0;
     
-    simd::float3 N = simd::normalize(disk.normal);
-    
-    simd::float3 T1 = (abs(N.x) > 0.5f)
-                       ? simd_make_float3(0, 1, 0)
-                       : simd_make_float3(1, 0, 0);
-    
-    simd::float3 T = simd::normalize(simd::cross(N, T1));
-    simd::float3 B = simd::normalize(simd::cross(T, N));
-
-    simd::float3x3 tbn(T, B, N);
+    simd::float3x3 tbn(disk.tangent, disk.bitangent, disk.normal);
     
     return tbn * simd::make_float3(x, y, z) + disk.origin;
 }
 
 simd_float2 mine::DiskCoordinates::getTextureCoordinates(const simd_float2& polarCoordinates, const Disk& disk) {
+    assert(disk.radius > 0.0f);
     float v = polarCoordinates[1] / disk.radius;
-    float u = (polarCoordinates[0] + M_PI) / (2 * M_PI);
+    float u = (polarCoordinates[0] + M_PI) / (2.0f * M_PI);
     return { u, v };
 }
 
--- mine/mine/scene/disk.h
+++ mine/mine/scene/disk.h
@@ -4,14 +4,18 @@
 
 #pragma once
 
+#include <cassert>
 #include <simd/simd.h>
 
 #include "../assertion/equal.h"
+#include "../general/normal.h"
 
 namespace mine {
     struct Disk {
         simd::float3 origin;
         simd::float3 normal;
+        simd::float3 tangent;
+        simd::float3 bitangent;
         float radius;
         
         Disk(simd::float3 const & origin,
@@ -20,7 +24,9 @@ namespace mine {
         : origin{origin}
         , normal{normal}
         , radius{radius} {
-            assertEqual(simd::length(normal), 1.0f);
+            assertEqual(simd::length(normal), 1.0f, 1e-4f);
+            assert(radius > 0.0f);
+            generateTBForNormal(tangent, bitangent, normal);
         }
     };
 }
