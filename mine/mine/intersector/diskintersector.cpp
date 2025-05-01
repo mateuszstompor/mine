c1d36b165b61f1aaf05398a4eb2312e48a999920
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-01 22:16:28 +0200

    Prevent division by 0

--- mine/mine/intersector/diskintersector.cpp
+++ mine/mine/intersector/diskintersector.cpp
@@ -8,6 +8,7 @@ std::optional<float> mine::DiskIntersector::intersect(const mine::Ray& ray,
                                                       const mine::Disk& disk) {
     float a = simd::dot(disk.origin - ray.origin, disk.normal);
     float b = simd::dot(ray.direction, disk.normal);
+    assert(b != 0);
     float t = a / b;
     
     simd_float3 point = ray.origin + ray.direction * t;
