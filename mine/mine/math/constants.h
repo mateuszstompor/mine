c7305ea7845a6db43cdb0ed98bf9b295ef90eeab
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-03 23:30:51 +0200

    Add pi constants

---
+++ mine/mine/math/constants.h
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2025 Mateusz Stompór. All rights reserved.
+//
+
+#pragma once
+
+namespace mine {
+    constexpr float INV_PI = 1.0f / M_PI;
+    constexpr float INV_TWO_PI = 1.0f / (2.0f * M_PI);
+    constexpr float TWO_PI = 2.0f * M_PI;
+}
