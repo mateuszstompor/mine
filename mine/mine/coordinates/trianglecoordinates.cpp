5c98a74ea79b36d9a9370c7c3f0947d8f56609ad
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-03 23:31:14 +0200

    Make triangle coords faster

--- mine/mine/coordinates/trianglecoordinates.cpp
+++ mine/mine/coordinates/trianglecoordinates.cpp
@@ -11,19 +11,25 @@ float mine::TriangleCoordinates::area(const simd::float3& v0, const simd::float3
 
 simd::float3 mine::TriangleCoordinates::barycentricCoordinates(const simd::float3& point,
                                                                const Triangle& triangle) const {
-    const simd::float3& v0 = triangle.v0;
-    const simd::float3& v1 = triangle.v1;
-    const simd::float3& v2 = triangle.v2;
-    
-    float abc = area(v0, v1, v2);
-    float bcp = area(v1, v2, point);
-    float cap = area(v2, v0, point);
-    float abp = area(v0, v1, point);
-    
-    float u = bcp / abc;
-    float v = cap / abc;
-    float w = abp / abc;
-    
+    const simd::float3& a = triangle.v0;
+    const simd::float3& b = triangle.v1;
+    const simd::float3& c = triangle.v2;
+
+    simd::float3 v0 = b - a;
+    simd::float3 v1 = c - a;
+    simd::float3 v2 = point - a;
+
+    float d00 = simd::dot(v0, v0);
+    float d01 = simd::dot(v0, v1);
+    float d11 = simd::dot(v1, v1);
+    float d20 = simd::dot(v2, v0);
+    float d21 = simd::dot(v2, v1);
+
+    float denom = d00 * d11 - d01 * d01;
+    float v = (d11 * d20 - d01 * d21) / denom;
+    float w = (d00 * d21 - d01 * d20) / denom;
+    float u = 1.0f - v - w;
+
     return simd::make_float3(u, v, w);
 }
 
@@ -31,25 +37,14 @@ simd::float3 mine::TriangleCoordinates::barycentricToCartesian(const simd::float
                                                                const simd::float3& v0,
                                                                const simd::float3& v1,
                                                                const simd::float3& v2) const {
-    float x1 = v0.x * x.x + v1.x * x.y + v2.x * x.z;
-    float y1 = v0.y * x.x + v1.y * x.y + v2.y * x.z;
-    float z1 = v0.z * x.x + v1.z * x.y + v2.z * x.z;
-    
-    return simd::make_float3(x1, y1, z1);
+    return v0 * x.x + v1 * x.y + v2 * x.z;
 }
 
 simd::float2 mine::TriangleCoordinates::getTextureCoordinates(const simd::float3& point,
                                                               const Triangle& triangle) const {
     simd::float3 barycentric = barycentricCoordinates(point, triangle);
-    
-    float u = 1.0 - barycentric.y - barycentric.z;
-    float v = 1.0 - barycentric.x - barycentric.z;
-    float w = 1.0 - barycentric.x - barycentric.y;
-    
-    simd::float3 cartesian = barycentricToCartesian(simd::make_float3(u, v, w),
-                                                     simd::make_float3(triangle.uv0, 0.0),
-                                                     simd::make_float3(triangle.uv1, 0.0),
-                                                     simd::make_float3(triangle.uv2, 0.0));
-    
-    return simd::make_float2(cartesian.x, cartesian.y);
+
+    return triangle.uv0 * barycentric.x +
+           triangle.uv1 * barycentric.y +
+           triangle.uv2 * barycentric.z;
 }
--- mine/mine/coordinates/trianglecoordinates.h
+++ mine/mine/coordinates/trianglecoordinates.h
@@ -11,15 +11,19 @@
 namespace mine {
     class TriangleCoordinates {
     public:
-        float area(const simd::float3& v0, const simd::float3& v1, const simd::float3& v2) const;
+        float area(const simd::float3& v0,
+                   const simd::float3& v1,
+                   const simd::float3& v2) const;
         
-        simd::float3 barycentricCoordinates(const simd::float3& point, const Triangle& triangle) const;
+        simd::float3 barycentricCoordinates(const simd::float3& point,
+                                            const Triangle& triangle) const;
         
         simd::float3 barycentricToCartesian(const simd::float3& x,
                                             const simd::float3& v0,
                                             const simd::float3& v1,
                                             const simd::float3& v2) const;
         
-        simd::float2 getTextureCoordinates(const simd::float3& point, const Triangle& triangle) const;
+        simd::float2 getTextureCoordinates(const simd::float3& point,
+                                           const Triangle& triangle) const;
     };
 }
