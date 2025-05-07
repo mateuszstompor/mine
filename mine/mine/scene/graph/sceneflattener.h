6db1f2cacb6cffdac2ff718277f1e7b014f4803a
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-07 14:49:36 +0200

    Concatenate matrices

--- mine/mine/scene/graph/sceneflattener.h
+++ mine/mine/scene/graph/sceneflattener.h
@@ -16,19 +16,25 @@ namespace mine {
         Scene flatten(SceneGraph const & graph) {
             Scene outputScene;
             if (graph.root != nullptr) {
-                process(graph.root, outputScene);
+                process(graph.root, matrix_identity_float4x4, outputScene);
             }
             outputScene.environmentMap = graph.environment;
             return outputScene;
         }
     private:
-        void process(std::unique_ptr<BaseNode> const & node, Scene & output) {
+        void process(std::unique_ptr<BaseNode> const & node, simd::float4x4 parentTransform, Scene & output) {
             if (Node<SphereNodeContents> * sphereNode = dynamic_cast<Node<SphereNodeContents>*>(node.get())) {
-                output.spheres.push_back(sphereNode->data.sphereObject);
+                SphereNodeContents const & contents = sphereNode->data;
+                parentTransform = contents.transform * parentTransform;
+                SphereObject outputObject = contents.sphereObject;
+                outputObject.sphere.center = (parentTransform * simd_make_float4(outputObject.sphere.center, 1.0f)).xyz;
+                output.spheres.push_back(outputObject);
+            } else if (Node<TransformNodeContents> * transformNode = dynamic_cast<Node<TransformNodeContents>*>(node.get())) {
+                TransformNodeContents const & contents = transformNode->data;
+                parentTransform = contents.transform * parentTransform;
             }
-            
             for (std::unique_ptr<BaseNode> const & child : node->getChildren()) {
-                process(child, output);
+                process(child, parentTransform, output);
             }
         }
     };
