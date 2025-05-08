e6a93df51964b6c41e83ac607cc27392a0420ece
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-09 00:14:20 +0200

    Add scale matrix

--- mine/mine/math/transform.h
+++ mine/mine/math/transform.h
@@ -12,6 +12,14 @@ namespace mine {
         matrix.columns[3].xyz = translation;
         return matrix;
     }
+    
+    inline simd::float4x4 scale(simd::float3 const & s) {
+        simd::float4x4 matrix = matrix_identity_float4x4;
+        matrix.columns[0].x = s.x;
+        matrix.columns[1].y = s.y;
+        matrix.columns[2].z = s.z;
+        return matrix;
+    }
 
     inline simd::float3x3 float4x4_to_float3x3(const simd::float4x4& mat4x4) {
         return simd::float3x3(
