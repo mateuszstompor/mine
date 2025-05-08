4583003159cb2cccdaa2ae1603931d18f64f9034
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-08 20:07:46 +0200

    Add vertex multiplication

--- mine/mine/scene/primitive/vertex.h
+++ mine/mine/scene/primitive/vertex.h
@@ -9,6 +9,7 @@
 #include "../../assertion/finite.h"
 #include "../../assertion/normalized.h"
 #include "../../assertion/perpendicular.h"
+#include "../../math/transform.h"
 
 namespace mine {
     struct Vertex {
@@ -41,4 +42,30 @@ namespace mine {
             assertPerpendicular(tangent, bitangent);
         }
     };
+}
+
+inline mine::Vertex operator*(simd::float4x4 const & matrix, mine::Vertex const & vertex) {
+    mine::assertNormalized(vertex.normal);
+    mine::assertNormalized(vertex.tangent);
+    mine::assertNormalized(vertex.bitangent);
+    
+    simd::float4 position4 = simd_make_float4(vertex.position, 1.0f);
+    position4 = matrix * position4;
+    
+    assert(position4.w == 1.0f);
+    
+    position4 /= position4.w;
+    
+    simd::float3x3 rotationMatrix = mine::float4x4_to_float3x3(matrix);
+    rotationMatrix = simd::transpose(simd::inverse(rotationMatrix));
+    mine::Vertex result = vertex;
+    result.normal = simd::normalize(rotationMatrix * result.normal);
+    result.tangent = simd::normalize(rotationMatrix * result.tangent);
+    result.bitangent = simd::normalize(rotationMatrix * result.bitangent);
+    result.position = position4.xyz;
+    
+    mine::assertNormalized(result.normal);
+    mine::assertNormalized(result.tangent);
+    mine::assertNormalized(result.bitangent);
+    return result;
 }
