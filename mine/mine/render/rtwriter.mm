9cc11c39d28a1c4e33f45380d5209455e92cef9d
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-10 14:13:07 +0200

    Fix sampling

--- mine/mine/render/rtwriter.mm
+++ mine/mine/render/rtwriter.mm
@@ -42,9 +42,10 @@ void mine::RTWriter::captureRegion(Region<uint16_t> const & region,
                                                     simd_make_float3(1, 1, 1));
             simd::float4 clampedRGBA = simd_make_float4(clampedColor, 1.0f);
             uint16_t flippedY = cgbitmap.bitmap.height - y - 1;
-            simd_float4 currentColor = cgbitmap.bitmap.colorAt(x, flippedY);
+            uint16_t flippedX = cgbitmap.bitmap.width - x - 1;
+            simd_float4 currentColor = cgbitmap.bitmap.colorAt(flippedX, flippedY);
             simd_float4 newColor = (currentColor * iteration + clampedRGBA) / float(iteration + 1);
-            cgbitmap.bitmap.setNormalizedRGBA(x, flippedY, newColor);
+            cgbitmap.bitmap.setNormalizedRGBA(flippedX, flippedY, newColor);
         }
     }
 }
