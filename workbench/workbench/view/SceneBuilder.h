287ed647e3064d1438deb5596671eab7bd315f27
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-11 20:07:33 +0200

    Mitsubishi scene

--- workbench/workbench/view/SceneBuilder.h
+++ workbench/workbench/view/SceneBuilder.h
@@ -328,6 +328,28 @@ public:
         loadedGraph.environment = Bitmap(simd_make_float4(1.0, 1.0, 1.0, 1.0));
         return SceneFlattener().flatten(loadedGraph);
     }
+        
+    static Scene buildMitsubishiGraph() {
+        NSBundle * bundle = [NSBundle mainBundle];
+        NSString * path = [bundle pathForResource:@"Mitsubishi_Lancer_Evolution_6___www.vecarz" ofType:@"usdz"];
+        ModelLoader loader;
+        SceneGraph loadedGraph = loader.load([path UTF8String]);
+        std::unique_ptr<BaseNode> & root = loadedGraph.root;
+        Node<TransformNodeContents> * transformNode = (Node<TransformNodeContents> *)(root.get());
+        simd::float4x4 s = scale(simd_make_float3(10, 10, 10));
+        float angle = -M_PI/2.0f;
+        simd_quatf q = simd_quaternion(angle, simd_make_float3(0, 1, 0));
+        simd_float3x3 rotMatrix = simd_matrix3x3(q);
+        simd_float4x4 M = simd_matrix(
+            simd_make_float4(rotMatrix.columns[0], 0.0f),
+            simd_make_float4(rotMatrix.columns[1], 0.0f),
+            simd_make_float4(rotMatrix.columns[2], 0.0f),
+            simd_make_float4(0.0f, 0.0f, 0.0f, 1.0f)
+        );
+        transformNode->data.transform = translation(simd_make_float3(0, -5, 30)) * s * M;
+        loadedGraph.environment = Bitmap(simd_make_float4(1.0, 1.0, 1.0, 1.0));
+        return SceneFlattener().flatten(loadedGraph);
+    }
     
     static Scene buildGraph() {
         SceneGraph g;
