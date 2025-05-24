e753ebab33e6248c45f81eb368ad7f249370197c
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-24 18:55:48 +0200

    Simplify calls

--- mine/mine/samplers/core/nearestsampler.h
+++ mine/mine/samplers/core/nearestsampler.h
@@ -13,12 +13,12 @@
 namespace mine {
     class NearestSampler: public Sampler {
     public:
-        simd::float4 sample(float u, float v, const mine::Bitmap * texture) override {
-            assertInClosedRange(u, 0.0f, 1.0f);
-            assertInClosedRange(v, 0.0f, 1.0f);
+        simd::float4 sample(simd::float2 const & uv, mine::Bitmap const * texture) override {
+            assertInClosedRange(uv.x, 0.0f, 1.0f);
+            assertInClosedRange(uv.y, 0.0f, 1.0f);
             
-            uint16_t x = (texture->width - 1) * u;
-            uint16_t y = (texture->height - 1) * v;
+            uint16_t x = (texture->width - 1) * uv.x;
+            uint16_t y = (texture->height - 1) * uv.y;
             
             return texture->get(x, y);
         }
