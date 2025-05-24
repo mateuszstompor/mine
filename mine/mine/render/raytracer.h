c91df6db110e4ad815fba30e2183361f00de0e47
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-05-24 14:04:07 +0200

    Use normals for light

--- mine/mine/render/raytracer.h
+++ mine/mine/render/raytracer.h
@@ -337,21 +337,21 @@ namespace mine {
                 return simd_make_float3(0.0f, 0.0f, 0.0f);
             }
             
-            if (closest->material == nullptr) {
-                return *(closest->lightColor);
-            }
-            
-            simd_float3 point = closest->point;
+            simd::float3 normal;
             simd_float2 uv = closest->uv;
-            simd::float3 normal = sampler.sample(uv[0], uv[1], closest->material->normal.get()).xyz;
-            normal = (normal * 2.0f) - 1.0f;
+            
+            if (closest->material == nullptr) {
+                normal = simd_make_float3(0.0f, 0.0f, 1.0f);
+            } else {
+                normal = sampler.sample(uv[0], uv[1], closest->material->normal.get()).xyz;
+                normal = (normal * 2.0f) - 1.0f;
+            }
             
             simd::float3x3 tbn(closest->T,
                                closest->B,
                                closest->N);
             
-            normal = simd::normalize(tbn * normal);
-            return normal;
+            return simd::normalize(tbn * normal);
         }
         simd_float3 traceAlbedo(Ray const & r,
                                 mine::Scene const & scene,
@@ -367,7 +367,6 @@ namespace mine {
                 return *(closest->lightColor);
             }
             
-            simd_float3 point = closest->point;
             simd_float2 uv = closest->uv;
             simd::float3 albedo = sampler.sample(uv[0], uv[1], closest->material->albedo.get()).xyz;
             return albedo;
